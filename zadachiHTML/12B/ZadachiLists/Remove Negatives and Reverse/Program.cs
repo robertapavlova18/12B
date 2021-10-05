e59f915ed41a444f07c2a8b2c69783a9dc351bf6
robertapavlova18 <87113717+robertapavlova18@users.noreply.github.com>
2021-10-05 09:32:50 +0300

    Add files via upload

---
+++ zadachiHTML/12B/ZadachiLists/Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Negatives_and_Reverse
+{
+    class Program
+    {
+        private static int item;
+
+        static void Main(string[] args)
+        {
+            var num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            List<int> numbers = new List<int>();
+
+            foreach (var item in num)
+            {
+                if (item > 0)
+                {
+                    numbers.Add(item);
+                }
+            }
+            foreach (var item in num)
+            {
+                if (item < 0)
+                {
+                    numbers.Remove(item);
+                }
+            }
+            numbers.Reverse();
+
+            if (numbers.Any()) { Console.WriteLine(string.Join(" ", numbers)); }
+            else { Console.WriteLine("empty"); }
+        }
+    }
+}
---
+++ zadachiHTML/12B/ZadachiLists/ZadachiLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZadachiLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
